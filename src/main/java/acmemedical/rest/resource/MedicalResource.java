933b78bf6a3b76ca5e1249300fe09f46bcc759de
Krish Chaudhary <161269571+Krish14505@users.noreply.github.com>
2024-11-28 18:09:38 -0500

    enter the crud operations methods to medicalService and start making resource class

--- src/main/java/acmemedical/rest/resource/MedicalResource.java
+++ src/main/java/acmemedical/rest/resource/MedicalResource.java
@@ -1,4 +1,30 @@
 package acmemedical.rest.resource;
 
+import acmemedical.ejb.ACMEMedicalService;
+import acmemedical.entity.Medicine;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ejb.EJB;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import static acmemedical.utility.MyConstants.*;
+
+@Path(MEDICINE_SUBRESOURCE_NAME)
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MedicalResource {
+
+    @EJB
+    protected ACMEMedicalService service;
+
+    @GET
+    @RolesAllowed({ADMIN_ROLE,USER_ROLE})
+    public Response getAllMedicines(){
+        List<Medicine> medicines = service.getAllMedicines();
+    }
+
 }
