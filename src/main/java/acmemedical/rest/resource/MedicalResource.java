feaef13f4bfc867da5d93d14beb660fc39445b11
Krish Chaudhary <161269571+Krish14505@users.noreply.github.com>
2024-11-28 18:38:58 -0500

    completed the implementation of the MedicalResource class

--- src/main/java/acmemedical/rest/resource/MedicalResource.java
+++ src/main/java/acmemedical/rest/resource/MedicalResource.java
@@ -4,10 +4,8 @@ import acmemedical.ejb.ACMEMedicalService;
 import acmemedical.entity.Medicine;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.ejb.EJB;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -24,9 +22,41 @@ public class MedicalResource {
     protected ACMEMedicalService service;
 
     @GET
-    @RolesAllowed({ADMIN_ROLE,USER_ROLE})
-    public Response getAllMedicines(){
+    @RolesAllowed({ADMIN_ROLE, USER_ROLE})
+    public Response getAllMedicines() {
         List<Medicine> medicines = service.getAllMedicines();
+        return Response.ok(medicines).build();
     }
 
+    @GET
+    @RolesAllowed({ADMIN_ROLE, USER_ROLE})
+    @Path(RESOURCE_PATH_ID_PATH)
+    public Response getMedicineById(@PathParam(RESOURCE_PATH_ID_ELEMENT) int id) {
+        Medicine medicine = service.getMedicineById(id);
+        return medicine != null ? Response.ok(medicine).build() : Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @POST
+    @RolesAllowed({ADMIN_ROLE})
+    public Response addMedicine(@Valid Medicine medicine) {
+        Medicine newMedicine = service.persistMedicine(medicine);
+        return Response.ok(newMedicine).build();
+    }
+
+    @PUT
+    @RolesAllowed({ADMIN_ROLE})
+    @Path(RESOURCE_PATH_ID_PATH)
+    public Response updateMedicine(@PathParam(RESOURCE_PATH_ID_ELEMENT) int id, Medicine medicine) {
+        Medicine updatingMedicine = service.updateMedicine(id, medicine);
+        return medicine != null ? Response.ok(updatingMedicine).build() : Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @DELETE
+    @RolesAllowed({ADMIN_ROLE})
+    @Path(RESOURCE_PATH_ID_PATH)
+    public Response deleteMedicine(@PathParam(RESOURCE_PATH_ID_ELEMENT) int id) {
+        service.deleteMedicine(id);
+        return Response.noContent().build();
+    }
 }
+
