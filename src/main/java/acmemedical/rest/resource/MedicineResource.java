51d61b1410802da630a214df7995910cc4246a02
Krish Chaudhary <161269571+Krish14505@users.noreply.github.com>
2024-12-02 12:43:39 -0500

    updated the medicine resource class to prevent the unauthorised user to access the method

--- src/main/java/acmemedical/rest/resource/MedicineResource.java
+++ src/main/java/acmemedical/rest/resource/MedicineResource.java
@@ -4,10 +4,15 @@ import acmemedical.ejb.ACMEMedicalService;
 import acmemedical.entity.Medicine;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.ejb.EJB;
+import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
+import jakarta.xml.ws.WebServiceContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
@@ -16,14 +21,20 @@ import static acmemedical.utility.MyConstants.*;
 @Path(MEDICINE_SUBRESOURCE_NAME)
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public class MedicalResource {
+public class MedicineResource {
+
+    private static final Logger LOG = LogManager.getLogger();
 
     @EJB
     protected ACMEMedicalService service;
 
+    @Inject
+    protected SecurityContext sc;
+
     @GET
-    @RolesAllowed({ADMIN_ROLE, USER_ROLE})
+    @RolesAllowed({ADMIN_ROLE})
     public Response getAllMedicines() {
+        LOG.debug("retrieving all medicines...");
         List<Medicine> medicines = service.getAllMedicines();
         return Response.ok(medicines).build();
     }
@@ -32,29 +43,27 @@ public class MedicalResource {
     @RolesAllowed({ADMIN_ROLE, USER_ROLE})
     @Path(RESOURCE_PATH_ID_PATH)
     public Response getMedicineById(@PathParam(RESOURCE_PATH_ID_ELEMENT) int id) {
+        LOG.debug("retrieving medicine by id: {}", id);
         Medicine medicine = service.getMedicineById(id);
-        return medicine != null ? Response.ok(medicine).build() : Response.status(Response.Status.NOT_FOUND).build();
-    }
+        return Response.status(medicine == null ? Response.Status.NOT_FOUND : Response.Status.OK)
+                .entity(medicine)
+                .build();    }
 
     @POST
     @RolesAllowed({ADMIN_ROLE})
-    public Response addMedicine(@Valid Medicine medicine) {
+    public Response addMedicine(Medicine medicine) {
+        LOG.debug("adding new medicine: {}", medicine);
         Medicine newMedicine = service.persistMedicine(medicine);
         return Response.ok(newMedicine).build();
     }
 
-    @PUT
-    @RolesAllowed({ADMIN_ROLE})
-    @Path(RESOURCE_PATH_ID_PATH)
-    public Response updateMedicine(@PathParam(RESOURCE_PATH_ID_ELEMENT) int id, Medicine medicine) {
-        Medicine updatingMedicine = service.updateMedicine(id, medicine);
-        return medicine != null ? Response.ok(updatingMedicine).build() : Response.status(Response.Status.NOT_FOUND).build();
-    }
+
 
     @DELETE
     @RolesAllowed({ADMIN_ROLE})
     @Path(RESOURCE_PATH_ID_PATH)
     public Response deleteMedicine(@PathParam(RESOURCE_PATH_ID_ELEMENT) int id) {
+        LOG.debug("deleting medicine by id: {}", id);
         service.deleteMedicine(id);
         return Response.noContent().build();
     }
